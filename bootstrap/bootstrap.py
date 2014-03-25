b7969c51d37ed5b53d83f87503536e3fc28541aa
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-03-25 15:26:35 +0100

    Print command line arguments.

--- bootstrap/bootstrap.py
+++ bootstrap/bootstrap.py
@@ -7,13 +7,14 @@
 __version__ = "0.2.0"
 
 
+import sys
 from .stuff import Stuff
 
 
 def main():
     print("Executing bootstrap version %s." % __version__)
-    print(Stuff)
-    print(Boo())
+    print("List of argument strings: %s" % sys.argv[1:])
+    print("Stuff and Boo():\n%s\n%s" % (Stuff, Boo()))
 
 
 class Boo(Stuff):
