daae8c00107521bb4a116f5b3184eb361f854aed
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2014-03-25 14:51:00 +0100

    setup.py: read long description from README.

--- setup.py
+++ setup.py
@@ -3,11 +3,17 @@
 import re
 from setuptools import setup
 
+
 version = re.search(
     '^__version__\s*=\s*"(.*)"',
     open('bootstrap/bootstrap.py').read(),
     re.M
     ).group(1)
+
+
+with open("README.rst", "rb") as f:
+    long_descr = f.read().decode("utf-8")
+
 
 setup(
     name = "cmdline-bootstrap",
@@ -17,6 +23,7 @@ setup(
         },
     version = version,
     description = "Python command line application bare bones template.",
+    long_description = long_descr,
     author = "Jan-Philip Gehrcke",
     author_email = "jgehrcke@googlemail.com",
     url = "http://gehrcke.de/2014/02/distributing-a-python-command-line-application",
